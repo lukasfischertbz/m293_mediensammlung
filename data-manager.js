a9ba87a505f1a0477a4ccbed52901319059fcefa
Jonas <nichtherrjonas@gmail.com>
2026-02-23 16:13:46 +0100

    Update data-manager.js

--- data-manager.js
+++ data-manager.js
@@ -9,29 +9,25 @@ const categoryTemplate = $("#category-template")?.content;
 const mainContainer = $("#main-content-list");
 const entryTemplate = $("#main-content-entry")?.content;
 
-{
-  const categorys = new Set(...Array(data.map((d) => d?.category)));
-  categorys.delete(null);
-  categoryContainer.replaceChildren(
-    ...Array.from(["alle", ...categorys]).map((name) => {
-      const category = categoryTemplate?.cloneNode(1);
-      category.querySelector("#label").innerText = String(name);
-      return category;
-    }),
-  );
-}
+const categorys = new Set(...Array(data.map((d) => d?.category)));
+categorys.delete(null);
+categoryContainer.replaceChildren(
+  ...Array.from(["alle", ...categorys]).map((name) => {
+    const category = categoryTemplate?.cloneNode(1);
+    category.querySelector("#label").innerText = String(name);
+    return category;
+  }),
+);
 
-{
-  const entrys = data.map((d) => d.contents).flat();
-  console.log(entrys);
-  mainContainer.replaceChildren(
-    ...entrys.map(({ title, image, length, src }) => {
-      const entry = entryTemplate?.cloneNode(1);
-      entry.querySelector("h2").src = title;
-      entry.querySelector("img").src = image;
-      entry.querySelector("a").href = src;
-      entry.querySelector("#length").innerText = length;
-      return entry;
-    }),
-  );
-}
+const entrys = data.map((d) => d.contents).flat();
+console.log(entrys);
+mainContainer.replaceChildren(
+  ...entrys.map(({ title, image, length, src }) => {
+    const entry = entryTemplate?.cloneNode(1);
+    entry.querySelector("#label").innerText = title;
+    entry.querySelector("img").src = image;
+    entry.querySelector("a").href = src ?? null;
+    entry.querySelector("#length").innerText = length ?? null;
+    return entry;
+  }),
+);
