46e58984a8d9c3ca8b67eebf035d43e0a66f3f5e
Jonas <nichtherrjonas@gmail.com>
2026-03-02 15:03:23 +0100

    new

--- data-manager.js
+++ data-manager.js
@@ -3,31 +3,16 @@ import data from "./data.json" with { type: "json" };
 
 const $ = (e) => document.querySelector(e);
 
-const categoryContainer = $("#category-container");
-const categoryTemplate = $("#category-template")?.content;
-
 const mainContainer = $("#main-content-list");
 const entryTemplate = $("#main-content-entry")?.content;
 
-const categorys = new Set(...Array(data.map((d) => d?.category)));
-categorys.delete(null);
-categoryContainer.replaceChildren(
-  ...Array.from(["alle", ...categorys]).map((name) => {
-    const category = categoryTemplate?.cloneNode(1);
-    category.querySelector("#label").innerText = String(name);
-    return category;
-  }),
-);
-
-const entrys = data.map((d) => d.contents).flat();
-console.log(entrys);
 mainContainer.replaceChildren(
-  ...entrys.map(({ title, image, length, src }) => {
+  ...data.map(({ title, image, release, src }) => {
     const entry = entryTemplate?.cloneNode(1);
     entry.querySelector("#label").innerText = title;
     entry.querySelector("img").src = image;
     entry.querySelector(".entry").dataset.source = src;
-    entry.querySelector("#length").innerText = length ?? null;
+    entry.querySelector("#release-date").innerText = release;
     return entry;
   }),
 );
